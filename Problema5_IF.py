8009eff03a1f206f5f45982fb1431b673a328b4d
elenamoglan <70642076+elenamoglan@users.noreply.github.com>
2020-10-17 21:35:55 +0300

    Delete Problema5_IF.py

--- Problema5_IF.py
+++
@@ -1,3 +0,0 @@
-'''Cunoscând data curentă exprimată prin trei numere întregi reprezentând anul, luna, ziua precum şi data naşterii unei persoane,
-exprimată la fel, să se facă un program care să calculeze vârsta persoanei respective în număr de ani împliniţi.'''
-
