5c048f587bf66cc564edfdecdad36a457c8a2c12
elenamoglan <70642076+elenamoglan@users.noreply.github.com>
2020-10-18 17:57:52 +0300

    Update Problema4_IF.py

--- Problema4_IF.py
+++ Problema4_IF.py
@@ -15,22 +15,3 @@ if (18<c<60):
     f = True
 if f == False:
     print('Toate varstele sunt ori mai mici sau egale cu 18 ori mai mare sau egale cu 60')
-
-
-'''
-if (18<a<60) and (18<b<60) and (18<c<60):
-    print(a, b, c)
-elif (18<a<60) and (18<b<60) and ((18>c) or (c>60)):
-    print(a, b)
-elif (18<a<60) and ((b<18) or (b>60)) and (18<c<60):
-    print(a, c)
-elif ((a<18) or (a>60)) and (18<b<60) and (18<c<60):
-    print(b, c)
-elif (18<a<60) and ((b<18) or(b>60)) and ((c<18) or (c>60)):
-    print(a)
-elif ((a<18) or (a>60)) and (18<b<60) and ((c<18) or (c>60)):
-    print(b)
-elif ((a<18) or (a>60)) and ((b<18) or(b>60)) and (18<c<60):
-    print(c)
-else:
-    print('Toate varstele sunt ori mai mici sau egale cu 18 ori mai mare sau egale cu 60')'''
